943b261d61e14b44711970e9fa98656b316a24de
Marcin Stanek <marcinstanek@MacBook-Pro-Marcin.local>
2018-11-05 01:47:50 +0100

    first commit

---
+++ tests/src/test/groovy/com/szkolqa/biolerplate/BaseSpec.groovy
@@ -0,0 +1,7 @@
+package com.szkolqa.biolerplate
+
+import spock.lang.Specification
+
+class BaseSpec extends Specification {
+
+}
---
+++ tests/src/test/groovy/com/szkolqa/biolerplate/InitTest.groovy
@@ -0,0 +1,11 @@
+package com.szkolqa.biolerplate
+
+class InitTest extends BaseSpec {
+    def 'first specifiacation'(){
+        when:
+        print 'hello world from first specifiacation'
+
+        then:
+        true
+    }
+}
